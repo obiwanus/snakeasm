5754bcb1df51d46fdcb185933d81d86f068989b0
Ivan Ivanov <ivan@ivanovs.info>
2019-08-05 09:51:42 +0200

    move the player around

--- c/main.cc
+++ c/main.cc
@@ -3,6 +3,11 @@
 
 #include <SDL2/SDL.h>
 
+struct Player {
+    int x;
+    int y;
+};
+
 int main(int argc, char *argv[])
 {
     SDL_Event event;
@@ -18,28 +23,57 @@ int main(int argc, char *argv[])
 
     SDL_Init(SDL_INIT_VIDEO);
     SDL_CreateWindowAndRenderer(kWindowSize, kWindowSize, 0, &window, &renderer);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-    SDL_Rect sdl_rect;
-    sdl_rect.w = kTileSize;
-    sdl_rect.h = kTileSize;
-    for (int y = 0; y < kTileCount; ++y) {
-        for (int x = 0; x < kTileCount; ++x) {
-            if ((float(rand()) / float(RAND_MAX)) < 0.5f) continue;
+    const int kTargetFPS = 60;
+    const Uint32 kFrameMS = 1000 / kTargetFPS;  // of course not precise
 
-            sdl_rect.x = kTileGapSize + x * (kTileSize + kTileGapSize);
-            sdl_rect.y = kTileGapSize + y * (kTileSize + kTileGapSize);
-            SDL_RenderFillRect(renderer, &sdl_rect);
-        }
-    }
+    Player player;
+    player.x = kTileCount / 2;
+    player.y = kTileCount / 2;
 
-    SDL_RenderPresent(renderer);
+    const Uint8 *keystate = SDL_GetKeyboardState(NULL);
 
     while (true) {
-        if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
-            break;
+        Uint32 start_time = SDL_GetTicks();
+
+        SDL_PumpEvents();
+        if (SDL_PollEvent(&event) && event.type == SDL_QUIT) break;
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+        // Update
+        if (keystate[SDL_SCANCODE_LEFT]) {
+            player.x -= 1;
+        }
+        if (keystate[SDL_SCANCODE_RIGHT]) {
+            player.x += 1;
+        }
+        if (keystate[SDL_SCANCODE_UP]) {
+            player.y -= 1;
+        }
+        if (keystate[SDL_SCANCODE_DOWN]) {
+            player.y += 1;
+        }
+
+        player.x = (player.x + kTileCount) % kTileCount;
+        player.y = (player.y + kTileCount) % kTileCount;
+
+        // Render
+        SDL_Rect sdl_rect;
+        sdl_rect.w = kTileSize;
+        sdl_rect.h = kTileSize;
+        sdl_rect.x = kTileGapSize + player.x * (kTileSize + kTileGapSize);
+        sdl_rect.y = kTileGapSize + player.y * (kTileSize + kTileGapSize);
+        SDL_RenderFillRect(renderer, &sdl_rect);
+
+        SDL_RenderPresent(renderer);
+
+        const Uint32 frame_time = (SDL_GetTicks() - start_time);
+        if (frame_time < kFrameMS) {
+            SDL_Delay(kFrameMS - frame_time);
+        }
     }
 
     SDL_DestroyRenderer(renderer);
