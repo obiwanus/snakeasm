d8ea8523be6336122f66a6d70d0e4458c15313d2
Ivan Ivanov <ivan@ivanovs.info>
2019-08-06 11:37:02 +0200

    Eat food

--- c/main.cc
+++ c/main.cc
@@ -3,6 +3,10 @@
 
 #include <SDL2/SDL.h>
 
+static const int kTileCount = 20;
+static const int kTileSize = 20;
+static const int kTileGapSize = 1;
+
 enum Direction {
     RIGHT = 0,
     LEFT,
@@ -23,6 +27,31 @@ struct Snake {
     v2 sections[MAX_SNAKE_SIZE];
 };
 
+void draw_rect(SDL_Renderer *renderer, v2 pos) {
+    SDL_Rect sdl_rect;
+    sdl_rect.w = kTileSize;
+    sdl_rect.h = kTileSize;
+    sdl_rect.x = kTileGapSize + pos.x * (kTileSize + kTileGapSize);
+    sdl_rect.y = kTileGapSize + pos.y * (kTileSize + kTileGapSize);
+    SDL_RenderFillRect(renderer, &sdl_rect);
+}
+
+v2 spawn_food(Snake *snake) {
+    while (true) {
+        v2 pos = {rand() % kTileCount, rand() % kTileCount};
+        bool collision = false;
+        for (int i = 0; i < snake->size; ++i) {
+            if (snake->sections[i].x == pos.x && snake->sections[i].y == pos.y) {
+                collision = true;
+                break;
+            }
+        }
+        if (!collision) {
+            return pos;
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     SDL_Event event;
@@ -31,9 +60,6 @@ int main(int argc, char *argv[])
 
     srand((int)time(NULL));
 
-    const int kTileCount = 20;
-    const int kTileSize = 20;
-    const int kTileGapSize = 1;
     const int kWindowSize = kTileCount * kTileSize + (kTileCount + 1) * kTileGapSize;
 
     SDL_Init(SDL_INIT_VIDEO);
@@ -56,6 +82,8 @@ int main(int argc, char *argv[])
 
     int frames_per_move = 30;  // determines game speed
     int frames_since_last_move = 30;
+
+    v2 food = spawn_food(&snake);
 
     // Frame loop
     while (true) {
@@ -84,7 +112,8 @@ int main(int argc, char *argv[])
             frames_since_last_move = 0;
 
             // Move the tail
-            for (int i = snake.size - 1; i > 0; --i) {
+            for (int i = snake.size; i > 0; --i) {
+                // Intentionally copy a hidden tail section in case we need to grow
                 snake.sections[i] = snake.sections[i - 1];
             }
 
@@ -109,18 +138,32 @@ int main(int argc, char *argv[])
             head->x = (head->x + kTileCount) % kTileCount;
             head->y = (head->y + kTileCount) % kTileCount;
 
+            // Maybe eat food
+            if (head->x == food.x && head->y == food.y) {
+                snake.size++;
+                if (frames_per_move > 1) {
+                    frames_per_move--;
+                }
+                if (snake.size < MAX_SNAKE_SIZE - 1) {
+                    food = spawn_food(&snake);
+                } else {
+                    food.x = -1;
+                    food.y = -1;
+                }
+            }
+
             // Render
             SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
             SDL_RenderClear(renderer);
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-            SDL_Rect sdl_rect;
-            sdl_rect.w = kTileSize;
-            sdl_rect.h = kTileSize;
+            // Food
+            SDL_SetRenderDrawColor(renderer, 255, 100, 100, 255);
+            draw_rect(renderer, food);
+
+            // Snake
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
             for (int i = 0; i < snake.size; ++i) {
-                sdl_rect.x = kTileGapSize + snake.sections[i].x * (kTileSize + kTileGapSize);
-                sdl_rect.y = kTileGapSize + snake.sections[i].y * (kTileSize + kTileGapSize);
-                SDL_RenderFillRect(renderer, &sdl_rect);
+                draw_rect(renderer, snake.sections[i]);
             }
 
             SDL_RenderPresent(renderer);
